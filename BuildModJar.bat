4ece4275b609108f1c7b756a4fd25bdcd18afffd
Murder-is-Tasty <mb.mattboehm@gmail.com>
2013-04-10 00:21:45 -0400

    Release Candidate 1
    Final commit before much testing via a dev build.

--- BuildModJar.bat
+++ BuildModJar.bat
@@ -1,7 +1,8 @@
 ::UNIVERSAL ELECTRICITY BUILD SCRIPT
 @echo off
-echo Promotion Type? x bugged * recommended @stable
+echo Promotion Type?
 set /p PROMOTION=
+
 
 set /p MODVERSION=<modversion.txt
 set /p CurrentBuild=<buildnumber.txt
@@ -13,6 +14,8 @@ if %PROMOTION%==* (
 )
 
 set FILE_NAME=MurderCoins_v%MODVERSION%.%BUILD_NUMBER%.jar
+set DEV_NAME=MCDev_v%MODVERSION%.%BUILD_NUMBER%.jar
+set BACKUP_NAME=MurderCoins_v%MODVERSION%.%BUILD_NUMBER%_backup.zip
 
 echo Starting to build %FILE_NAME%
 
@@ -22,15 +25,29 @@ runtime\bin\python\python_mcp runtime\reobfuscate.py %*
 
 ::ZIP-UP
 cd reobf\minecraft\
-"..\..\..\7za.exe" a "..\..\builds\%FILE_NAME%" "murder"
-"..\..\..\7za.exe" a "..\..\builds\%FILE_NAME%" "universalelectricity"
+7z a "..\..\builds\%FILE_NAME%" "MurderCoins\"
 cd ..\..\
-cd src\minecraft\
-cd resources\mods\
-"..\..\..\7za.exe" a "..\..\builds\%FILE_NAME%" "*"
-cd ..\..\
+
+cd eclipse\Minecraft\bin\
+7z a "..\..\..\builds\%DEV_NAME%" "MurderCoins"
+cd ..\..\..\
+
+cd resources\
+7z a "..\builds\%FILE_NAME%" "*"
+7z a "..\builds\%API_NAME%" "*"
+7z a "..\builds\%DEV_NAME%" "*"
+7z a "..\builds\%BACKUP_NAME%" "*"
+cd ..\
+cd src\
+7z a "..\builds\%API_NAME%" "*\MurderCoins\"
+
+7z a "..\builds\%BACKUP_NAME%" "*\MurderCoins\"
+cd ..\
+
 ::UPDATE INFO FILE
-echo %PROMOTION% %FILE_NAME% %API_NAME%>>info1.5.1.txt
+echo %PROMOTION% %API_NAME% %FILE_NAME% %DEV_NAME%>>info.txt
+
+
 
 echo Done building %FILE_NAME%
 
